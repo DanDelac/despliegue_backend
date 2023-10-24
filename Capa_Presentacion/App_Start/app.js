237cd95cc20744d90d17690c5990b5f7ec5d902c
Proyectos <TallerProII@hotmail.com>
2023-10-23 21:00:14 -0500

    Despliegue primer incremento

--- Capa_Presentacion/App_Start/app.js
+++ Capa_Presentacion/App_Start/app.js
@@ -22,8 +22,8 @@ app.use(express.json());
 app.use("/", employeesRoutes);
 app.use("/citas", citasRoutes );
 
-app.use((req, res, next) => {
-  res.status(404).json({ message: "Not found" });
-});
+// app.use((req, res, next) => {
+//   res.status(404).json({ message: "Not found" });
+// });
 
 export default app;
