ffa239c19d846058dd07d07c5b95a666136a750d
Proyectos <TallerProII@hotmail.com>
2023-10-23 20:45:36 -0500

    Despliegue primer incremento

--- Capa_Presentacion/App_Start/app.js
+++ Capa_Presentacion/App_Start/app.js
@@ -8,15 +8,13 @@ import citasRoutes from "../Rutas/citas.routes.js";
 const app = express();
 
 // Middlewares
+app.use(cors()); //Habilitar CORS
 app.use(morgan("dev"));
 app.use(express.json());
 
 // Routes
 app.use("/", employeesRoutes);
 app.use("/citas", citasRoutes );
-
-// Habilitar el middleware CORS
-app.use(cors());
 
 app.use((req, res, next) => {
   res.status(404).json({ message: "Not found" });
