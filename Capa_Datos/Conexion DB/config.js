ec3aec0ef4f8daef8640e7756781fbd9ca103788
LAB_F202 <72113166@continental.edu.pe>
2023-11-20 16:42:14 -0500

    Actualizacción cn_citas listCita-updateCita

--- Capa_Datos/Conexion DB/config.js
+++ Capa_Datos/Conexion DB/config.js
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 config();
 
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 5000;
 export const DB_HOST = process.env.DB_HOST || "server-mysql202302.mysql.database.azure.com";
 export const DB_USER = process.env.DB_USER || "GeovanyDelaCruz";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "TallerProyect2023";
