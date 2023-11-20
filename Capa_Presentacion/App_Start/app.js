b254b70931caba903decf2259d17f33f0c5e3adc
LAB_F202 <72113166@continental.edu.pe>
2023-11-20 18:45:39 -0500

    Se agrego CRUD tratamiento

--- Capa_Presentacion/App_Start/app.js
+++ Capa_Presentacion/App_Start/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 
 import employeesRoutes from "../Rutas/employees.routes.js";
 import citasRoutes from "../Rutas/citas.routes.js";
+import tratamiento from "../Rutas/tratamiento.routes.js";
 
 const app = express();
 
@@ -11,6 +12,7 @@ const corsOptions = {
   origin: '*', // o '*' para permitir desde cualquier origen
   methods: 'GET,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
+  
 };
 
 // Middlewares
@@ -21,6 +23,7 @@ app.use(express.json());
 // Routes
 app.use("/", employeesRoutes);
 app.use("/citas", citasRoutes );
+app.use("/tratamiento", tratamiento );
 
 app.use((req, res, next) => {
   res.status(404).json({ message: "Not found" });
