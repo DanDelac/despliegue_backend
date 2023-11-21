f72d040bbeeba391d527e0336a594f375ed01571
Galvan <72113166@continental.edu.pe>
2023-11-20 21:24:36 -0500

    Actualizar cita

--- Capa_Presentacion/Rutas/citas.routes.js
+++ Capa_Presentacion/Rutas/citas.routes.js
@@ -8,16 +8,13 @@ import {
 
 const router = Router();
 
-
+//Listar cita
+router.get("/listCita", listCita);
 // Crear una cita
 router.post("/createCita", createCita);
 //Actualizar cita
 router.patch("/updateCita/:IDCita", updateCita);
-//Listar cita
-router.get("/listCita", listCita);
 // DELETE un empleado
 router.delete("/deleteCita/:IDCita", deleteCita);
-
-
 
 export default router;
