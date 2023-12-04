104b0ffe25b56f424989ea4a66badbde921af36f
LAB_F202 <72113166@continental.edu.pe>
2023-12-04 16:28:31 -0500

    Solucion row en medico

--- Capa_Presentacion/Controladores/medico.controller.js
+++ Capa_Presentacion/Controladores/medico.controller.js
@@ -6,7 +6,7 @@ var objCapaNegocio = new CN_medico();
 export const listmedico = async (req, res) => {
   try {
     const respuesta = await objCapaNegocio.listmedico();
-    res.json(respuesta);
+    res.json(respuesta["rows"]);
   } catch (error) {
     return res.status(500).json({ message: "Algo salió mal en CP - " + error });
   }
